0ca2d9bf93f3cf573b1d67bd2eca981ae0a451f5
jcanjosjr <josec.dosanjos@gmail.com>
2022-01-19 22:50:42 -0300

    day4

---
+++ Day4/1.randomModule.py
@@ -0,0 +1,13 @@
+import random
+
+randomInt = random.randint(1, 10)
+print(randomInt)
+
+randomFloat = random.random()
+print(randomFloat)
+
+randomFloat = random.random() * 5
+print(randomFloat)
+
+loveScore = random.randint(1, 100)
+print(f'Your love score is {loveScore}')
