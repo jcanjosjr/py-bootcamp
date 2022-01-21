f0e5c6a70ff5fb613c4853b8944ddb8f85fb19fd
jcanjosjr <josec.dosanjos@gmail.com>
2022-01-20 23:20:40 -0300

    update camelCase

---
+++ Day5/1.forLoop.py
@@ -0,0 +1,8 @@
+# What a itereable do?
+# It's this list of fruits and it's a signing a variable
+# name, fruit, to each of them.
+
+fruits = ['Apple', 'Peach', 'Pear']
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
