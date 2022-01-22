a6b698156a722a25288fc5223d76533f10fd306f
jcanjosjr <josec.dosanjos@gmail.com>
2022-01-22 10:21:45 -0300

    organization

---
+++ Beginner/Day6/1.functions.py
@@ -0,0 +1,15 @@
+print('Hello')
+numChar = len('Hello')
+print(numChar)
+
+# Once you've defined the function, you've created the recipe,
+# the next step is to actually use it, on programming language
+# we would say calling the function.
+
+
+def myFunction():
+    print('Hello')
+    print('Bye')
+
+
+myFunction()
