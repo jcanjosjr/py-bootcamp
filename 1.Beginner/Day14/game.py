c1b6464d73101185693310db0731667b7aaa38bc
jcanjosjr <josec.dosanjos@gmail.com>
2022-02-01 21:44:52 -0300

    comments add

--- 1.Beginner/Day14/game.py
+++ 1.Beginner/Day14/game.py
@@ -6,13 +6,15 @@ import random
 import click
 
 
+# Getting a random data on archive.
 def randData(text):
     """Get a random metadata on the archive."""
     return random.choice(text)
 
 
+# Formating the output from the Dict format.
 def formatData(text):
-    """Format the List[{Dict}] into a String."""
+    """Format the {'Dict': Format} into a readable String."""
     name = text["name"]
     desc = text["description"]
     country = text["country"]
@@ -23,16 +25,21 @@ def game():
     """Start the game, and compare the stars!"""
     recursive = True
     i = 0
+    # I like to use recursive for calling again the same "recurse"
     while recursive:
+        # Basically, I just this to repeat one time.
         if i == 0:   
             score = 0
             personA = randData(data)
             print(logo)
+        
+        # Defining, person Two and printing the "menu".
         personB = randData(data)
         print(f"Compare A: {formatData(personA)}.")
         print(vs)
         print(f"Compare B: {formatData(personB)}.")
         guess = input("Who has more followers? Type [A] or [B]: ")
+        # Comparising the persons, clear the Output and logic for results.
         if personA['follower_count'] > personB['follower_count'] and guess == 'A':
             click.clear()
             score += 1
@@ -50,6 +57,9 @@ def game():
             click.clear()
             print(logo)
             print(f"        Sorry, that's wrong. Final score: {score}. :(")
+            # Breaking the loop just where I want to finally.
             recursive = False
 
+
+# Starting the game.
 game()
