0240f2e3a68b4fd4c57ebdfd51e997998e72348f
jcanjosjr <josec.dosanjos@gmail.com>
2022-02-01 21:37:22 -0300

    maybe?

--- 1.Beginner/Day14/game.py
+++ 1.Beginner/Day14/game.py
@@ -3,6 +3,7 @@
 from art import logo, vs
 from data import data
 import random
+import click
 
 
 def randData(text):
@@ -20,10 +21,32 @@ def formatData(text):
 
 def game():
     """Start the game, and compare the stars!"""
-    personA = randData(data)
-    personB = randData(data)
-    print(f"Compare A: {formatData(personA)}")
-    print(f"Compare B: {formatData(personB)}")
-
+    recursive = True
+    i = 0
+    while recursive:
+        if i == 0:   
+            score = 0
+            personA = randData(data)
+        personB = randData(data)
+        print(logo)
+        print(f"Compare A: {formatData(personA)}.")
+        print(vs)
+        print(f"Compare B: {formatData(personB)}.")
+        guess = input("Who has more followers? Type [A] or [B]: ")
+        if personA['follower_count'] > personB['follower_count'] and guess == 'A':
+            score += 1
+            print(f"You're right! Current score: {score}")
+            i += 1
+            click.clear()
+        elif personA['follower_count'] < personB['follower_count'] and guess == 'B':
+            score += 1
+            print(f"You're right! Current score: {score}")
+            i += 1
+            personA = personB
+            click.clear()
+        else:
+            print(f"Sorry, that's wrong. Final score: {score}.")
+            recursive = False
+            click.clear()
 
 game()
