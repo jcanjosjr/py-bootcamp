6ed6973fecfab6b2e21ff37046472d32da45ceef
jcanjosjr <josec.dosanjos@gmail.com>
2022-02-13 11:38:56 -0300

    day16

--- 2.Intermediate/Day-B/3.pip.py
+++ 2.Intermediate/Day-B/3.pip.py
@@ -1,2 +1,14 @@
-import prettytable
+from prettytable import PrettyTable
 
+# Creating a new Object:
+table = PrettyTable() 
+
+# Using the method add_column from the object table.
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+# We can also change the object's attributes, like align to the left:
+table.align = "l"
+
+# Order the table from the column "Pokemon Name"
+print(table.get_string(sortby="Pokemon Name"))
